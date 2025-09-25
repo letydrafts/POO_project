78a915882a041f38dc2426c9f83a35dcefd70da4
letydrafts <giovannaleticia02203@gmail.com>
2025-09-25 09:29:25 -0300

    reloading

--- ecommerce/src/main/java/com/letydrafts/Utils/JavalinUtils.java
+++
@@ -1,8 +0,0 @@
-package com.letydrafts.Utils;
-
-public class JavalinUtils {
-    public void getcontext() {
-        
-    }
-    
-}
--- ecommerce/src/main/java/com/letydrafts/models/abstracts/Payment.java
+++
@@ -1,7 +0,0 @@
-package com.letydrafts.models.abstracts;
-
-public abstract class Payment {
-	private double valor;
-
-    public abstract void processPayment();
-}
